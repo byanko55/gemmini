c5d21dc0fdcd9451417ffcb94aa3dbaf9775c122
byanko55 <byanko55@gmail.com>
2024-02-14 16:35:22 +0900

    rotate_2D, polar_pixels

--- gemmini/calc/coords.py
+++ gemmini/calc/coords.py
@@ -1,11 +1,5 @@
 from gemmini.misc import *
 
-__all__ = [
-    "dist",
-    "_isNumber",
-    "_isPoint"
-]
-
 def dist(p:Tuple[Any, ...], q:Tuple[Any, ...]) -> float:
     """
     Returns the Euclidean distance between two points (p and q), where p and q are the coordinates of that point.
@@ -23,8 +17,64 @@ def dist(p:Tuple[Any, ...], q:Tuple[Any, ...]) -> float:
     
     return np.linalg.norm(np.array(p) - np.array(q))
 
-def _isNumber(n):
-    return isinstance(n, (int, float, np.number)) 
+def polar_pixels(r:Union[float, np.ndarray], theta:Union[float, np.ndarray]) -> np.ndarray:
+    """
+    Transforms polar coordinates (r, θ) into cartesian coordinates (x, y).
+    
+    Args:
+        r (float | np.ndarray): radius
+        theta (float | np.ndarray): angle (in radian)
+
+    Returns:
+        coord (np.ndarray): coordinates on cartesian system.
+    """
+    
+    if _isNumber(theta):
+        coord = np.array([[r*cos(theta), r*sin(theta)]])
+    elif _isNumberArray(theta) :
+        coord = np.stack((r*np.cos(theta), r*np.sin(theta)), axis=1)
+    else :
+        raise ValueError(" \
+            [ERROR] polar_pixels: radius/theta should be a floating value or list of numbers, \
+        ")
+    
+    return coord
+
+def rotate_2D(xy:Union[Tuple, List, np.ndarray], theta:float) -> Union[Tuple, np.ndarray]:
+    """
+    Rotate points in the xy plane anti-clockwise through an angle `theta`
+
+    Args:
+        xy (tuple | np.ndarray): collection of coordinates on 2D space.
+        theta (float): rotation angle (in radian)
+    """
+    if _isPoint(xy):
+        x, y = xy
+        xx = x * cos(theta) - y * sin(theta)
+        yy = x * sin(theta) + y * cos(theta)
+
+        return xx, yy
+    
+    if isinstance(xy, list):
+        xy = np.array(xy)
+    
+    c, s = np.cos(theta), np.sin(theta)
+    r = np.array([[c, -s], [s, c]])
+    m = np.dot(xy, r)
+
+    return m
+
+def _isNumber(n) -> bool:
+    return isinstance(n, (int, float, np.number))
+
+def _isNumberArray(a) -> bool:
+    if isinstance(a, np.ndarray) and len(a.shape) == 1 and _isNumber(a[0]):
+        return True
+    
+    if isinstance(a, list) and not isinstance(a[0], list) and _isNumber(a[0]):
+        return True
+    
+    return False
 
 def _isPoint(p:Tuple[Any, ...], dim:int = None) -> bool:
     if not isinstance(p, (tuple, list, np.ndarray)):
--- gemmini/test/calc/test_coords.py
+++ gemmini/test/calc/test_coords.py
@@ -1,5 +1,5 @@
 from gemmini.misc import *
-from gemmini.calc.coords import *
+from gemmini.calc.coords import _isNumber, _isPoint, dist, polar_pixels, rotate_2D
 
 if __name__ == "__main__":
     assert _isNumber(3) == True
@@ -20,3 +20,30 @@ if __name__ == "__main__":
     assert dist(p=[0,0], q=[3,0]) == 3
     assert dist(p=[0,0], q=[3,4]) == 5
     assert dist(p=[0,1,2], q=[1,3,4]) == 3
+
+    _o = polar_pixels(r=2, theta=pi/4)
+    assert _o[0][0] == 2*cos(pi/4)
+    assert _o[0][1] == 2*sin(pi/4)
+
+    theta = np.linspace(0, np.pi/2, 4)
+    _o = polar_pixels(10, theta)
+    assert _o[1][0] == 10*cos(pi/6)
+    assert _o[1][1] == 10*sin(pi/6)
+    assert _o[2][0] == 10*cos(pi/3)
+    assert _o[2][1] == 10*sin(pi/3)
+
+    _x, _y = rotate_2D((2, 0), np.pi/2)
+    assert (_x - 0.0) < 1e-6
+    assert (_y - 2.0) < 1e-6
+
+    _x, _y = rotate_2D(np.array([1, 1]), np.pi/2)
+    assert (_x - (-1.0)) < 1e-6
+    assert (_y - 1.0) < 1e-6
+
+    _x, _y = rotate_2D([2, 1], np.pi)
+    assert (_x - (-2.0)) < 1e-6
+    assert (_y - (-1.0)) < 1e-6
+
+    _o = rotate_2D(np.array([[1, 1], [3, 0]]), np.pi/3)
+    assert _o[1][0] - 3*cos(np.pi/3) < 1e-6
+    assert _o[1][1] - 3*sin(np.pi/3) < 1e-6
