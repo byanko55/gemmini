aa6fcd75382456c40dfcd06c3499dbc920f59018
byanko55 <byanko55@gmail.com>
2024-02-14 18:14:05 +0900

    adjust canvas size

--- gemmini/plot.py
+++ gemmini/plot.py
@@ -18,16 +18,20 @@ def plot(gem:List[Any]) -> None:
         x_min, y_min, x_max, y_max = gem.bounding_box()
         x_mean, y_mean = gem.center()
 
-        canvas_size = 10 ** (int(log10(max(1, max(x_max-x_min, y_max-y_min)))) + 1)
+        canvas_size = 2 ** (int(log2(max(1, max(x_max-x_min, y_max-y_min)))) + 1)
         lb, rb = int(x_mean) - canvas_size//2, int(x_mean) + canvas_size//2
         bb, tb = int(y_mean) - canvas_size//2, int(y_mean) + canvas_size//2
 
         ax.set_xlim([lb, rb])
         ax.set_ylim([bb, tb])
-        ax.set_xticks(list(range(lb, rb + 1, canvas_size//5)))
-        ax.set_yticks(list(range(bb, tb + 1, canvas_size//5)))
-        ax.set_xticks(range(lb, rb + 1, canvas_size//10), minor=True)
-        ax.set_yticks(range(bb, tb + 1, canvas_size//10), minor=True)
+        ax.set_xticks(np.linspace(lb, rb, 6))
+        ax.set_yticks(np.linspace(bb, tb, 6))
+        ax.set_xticks(np.linspace(lb, rb, 11), minor=True)
+        ax.set_yticks(np.linspace(bb, tb, 11), minor=True)
+        #ax.set_xticks(list(range(lb, rb + 1, canvas_size//5)))
+        #ax.set_yticks(list(range(bb, tb + 1, canvas_size//5)))
+        #ax.set_xticks(range(lb, rb + 1, canvas_size//10), minor=True)
+        #ax.set_yticks(range(bb, tb + 1, canvas_size//10), minor=True)
         ax.grid(which='both', color='#BDBDBD', linestyle='--', linewidth=1)
 
         plt.scatter(xs, ys, zorder=10)
