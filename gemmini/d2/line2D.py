bc4e6e8002de4b60259ff588b052a97afd59ec89
byanko55 <byanko55@gmail.com>
2024-02-19 13:50:57 +0900

    new feature: Line2D grad/parallel/orthog/intersect

--- gemmini/d2/line2D.py
+++ gemmini/d2/line2D.py
@@ -36,3 +36,61 @@ class Line2D:
         self.p1 = p1
         self.p2 = p2
         self.slope = slope
+
+    def grad(self) -> float:
+        """
+        Return the gradient of the line
+        """
+        if self.slope != None:
+            return self.slope
+
+        if self.p1[0] == self.p2[0]:
+            return inf
+
+        return (self.p2[1]-self.p1[1])/(self.p2[0]-self.p1[0])
+    
+    def parallel(self, other) -> bool:
+        """
+        True, if two line are parallel
+        """
+        if type(other) != Line2D:
+            raise ValueError("[Error] parallel: the input is not a `Line2D` object")
+
+        return self.grad() == other.grad()
+    
+    def orthog(self, other) -> bool:
+        """
+        True, if two line are orthogonal
+        """
+        if type(other) != Line2D:
+            raise ValueError("[Error] orthognal: the input is not a `Line2D` object")
+
+        ga = self.grad()
+        gb = other.grad()
+
+        if (ga == inf and gb == 0) or (ga == 0 and gb == inf):
+            return True
+        
+        if ga*gb == -1:
+            return True
+ 
+        return False
+    
+    def intersect(self, other) -> Tuple[float, float]:
+        """
+        Return the coordinates of intersection point of two lines
+        """
+        if self.parallel(other):
+            warnings.warn("[WARN] intersect: given two line are parallel")
+            return None
+        
+        ga = self.grad()
+        gb = other.grad()
+
+        rx = (ga*self.p1[0] - gb*self.p2[0] - self.p1[1] + self.p2[1])/(ga-gb)
+        ry = ga*(rx - self.p1[0]) + self.p1[1]
+
+        return rx, ry
+
+    def __and__(self, other) -> Tuple[float, float]:
+        return self.intersect(other)
--- gemmini/test/d2/test_line2D.py
+++ gemmini/test/d2/test_line2D.py
@@ -7,10 +7,12 @@ import unittest
 class SelfTests(unittest.TestCase): 
     def line1(self):
         l = Line2D((1,0), slope=1.5)
+        self.assertTrue(l.grad() == 1.5)
         plot(l)
 
     def line2(self):
         l = Line2D((1,0), (2,2))
+        self.assertTrue(l.grad() == 2)
         plot(l)
 
     def line_invalid1(self):
@@ -25,6 +27,22 @@ class SelfTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             l = Line2D((1,0), slope='1.5')
 
+    def two_lines(self):
+        a = Line2D((0, 0), slope=2)
+        b = Line2D((0, 1), slope=2)
+        c = Line2D((0, 0), slope=-1/2)
+        d = Line2D((2, 2), (2, -2))
+        e = Line2D((3, 0), (-3, 0))
+
+        self.assertTrue(a.parallel(b))
+        self.assertFalse(a.parallel(c))
+        self.assertTrue(a.orthog(c))
+        self.assertTrue(d.orthog(e))
+
+        x, y = d.intersect(e)
+        self.assertTrue(x == 2 and y == 0)
+        x, y = d & e
+        self.assertTrue(x == 2 and y == 0)
 
 if __name__ == '__main__':  
     unittest.main()
