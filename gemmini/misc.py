a3e3feac66292489a04f9cc52798d7e1adefc65a
byanko55 <byanko55@gmail.com>
2024-02-21 23:46:15 +0900

    fix: type comparison with Nonetype raise Error

--- gemmini/misc.py
+++ gemmini/misc.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.path as pth
 
 import warnings
-from math import sqrt, cos, sin, tan, pi, inf, log10
+from math import sqrt, cos, sin, tan, pi, inf, exp, log, log10
 from typing import Callable, Any, List, Optional, Tuple, Union
 
 COORDINATES = Union[Tuple, List, np.ndarray]
@@ -30,15 +30,15 @@ def assignArg(gem_type:str, list_abbrs:List[Any], list_full_names:List[str], kwa
         elif isinstance(full_name, str) and full_name in kwargs:
             arg_name = full_name
 
-        if abbr != None and arg_name != '':
+        if type(abbr) != type(None) and arg_name != '':
             raise ValueError("[ERROR] %s: same flag used two times"%(gem_type))
         
-        if abbr == None and arg_name == '':
+        if type(abbr) == type(None) and arg_name == '':
             raise ValueError("\
                 [ERROR] %s: '%s' argument was not given \
             "%(gem_type, full_name if type(full_name) == str else full_name[0]))
         
-        v = abbr if abbr != None else kwargs[arg_name]
+        v = abbr if type(abbr) != type(None) else kwargs[arg_name]
         res.append(v)
     
     if len(res) == 1:
