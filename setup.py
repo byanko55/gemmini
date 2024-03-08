ee7d7dc7d9224a341f402275febfa9aebd1b24d8
byanko55 <byanko55@gmail.com>
2024-03-08 14:40:54 +0900

    version 1.0.2

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
+
+with open('README.md', encoding='utf-8') as f:
+    readme = f.read()
 
 if __name__ == "__main__":
     setup(
         name='gemmini',
         version=__version__,
+        long_description=readme,
+        long_description_content_type='text/markdown',
         description='Python package for constructing and handling geometric objects',
         author='Yankos',
         author_email='byanko55@gmail.com',
